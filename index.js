ab534b42325d901567ffe703612e8b8c1bb045b3
Airuop <114985398+Airuop@users.noreply.github.com>
2023-07-23 19:07:32 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const Worker = require('./output.txt');
 const num1 = 10;
-const num2 = 220;
+const num2 = 2230;
 
 const sum = num1 + num2;
 
-console.log(`The sum is: ${sum}`);
+console.log(`The sum is: ${sum} az qabl ${Worker} l`);
