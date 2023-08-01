1002290759ce9b573b5191e034621e489b2d7607
Airuop <114985398+Airuop@users.noreply.github.com>
2023-08-01 10:02:51 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-const fs = require('fs');
-const Worker = fs.readFileSync('./output.txt',
-    { encoding: 'utf8', flag: 'r' });
 
 const num1 = 10;
-const num2 = 2230;
-
+const num2 = 20;
 const sum = num1 + num2;
-
-console.log(`The sum is: ${sum} az qabl ${Worker} l`);
+console.log(`The sum is: ${sum}`);
