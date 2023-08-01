27807b20f0654067cf211a18822577532d600450
Airuop <114985398+Airuop@users.noreply.github.com>
2023-08-01 10:00:29 +0330

    Update backup.py

--- backup.py
+++ backup.py
@@ -1,32 +1,29 @@
-import re
-import time
 import os
 import requests
+from datetime import datetime
 
 update_path = './update/'
 
 url = 'https://raw.githubusercontent.com/yebekhe/TelegramV2rayCollector/main/sub/reality'
 data = requests.get(url).text
 
-def backup(file):
-    try:
-        t = time.localtime()
-        date = time.strftime('%y%m', t)
-        date_day = time.strftime('%y%m%d', t)
 
-        file_eternity = open(file, 'r', encoding='utf-8')
-        sub_content = file_eternity.read()
-        file_eternity.close()
+def backup(data):
+  date_dir = datetime.now().strftime("%y%m")
+  date_file = datetime.now().strftime("%y%m%d_%H%M")
 
-        try:
-            os.mkdir(f'{update_path}{date}')
-        except FileExistsError:
-            pass
-        txt_dir = update_path + date + '/' + date_day + '.txt'  # 生成$MM$DD.txt文件名
-        file = open(txt_dir, 'w', encoding='utf-8')
-        file.write(sub_content)
-        file.close()
-    except Exception as e:
-        print("Error While backup")
+  try:
+    os.makedirs(f"{update_path}/{date_dir}", exist_ok=True)
+  except OSError:
+    print("Error creating backup directory")
+    return
+
+  file_path = f"{update_path}/{date_dir}/{date_file}.txt"
+
+  try:  
+    with open(file_path, "w", encoding="utf-8") as f:
+      f.write(data)
+  except OSError:
+    print("Error writing backup file")
 
 backup(data)
