e709d396c7586f1a1d3537c7d7268eb32c4f3a42
Airuop <114985398+Airuop@users.noreply.github.com>
2023-08-23 14:14:04 +0330

    Create backup4.py

---
+++ tools/backup4.py
@@ -0,0 +1,37 @@
+import os
+import requests
+import base64
+import re
+from datetime import datetime
+
+update_path = './countries/ir/'
+
+url = "https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/countries/ir/mixed"
+res = requests.get(url)
+encoded_str = res.content
+
+decoded_bytes = base64.b64decode(encoded_str)
+decoded_str = decoded_bytes.decode('utf-8') 
+
+# Remove unsupported characters before encoding
+cleaned_str = re.sub(r'[^\x00-\x7F]+', '', decoded_str)  
+
+def backup(response):
+  date_dir = datetime.now().strftime("%y%m")
+  date_file = datetime.now().strftime("%y%m%d_%H%M")
+
+  try:
+    os.makedirs(f"{update_path}/{date_dir}", exist_ok=True)
+  except OSError:
+    print("Error creating backup directory")
+    return
+
+  file_path = f"{update_path}/{date_dir}/{date_file}.txt"
+
+  try:  
+    with open(file_path, "w", encoding="utf-8") as f:
+            f.write(response)
+  except OSError:
+    print("Error writing backup file")
+
+backup(cleaned_str)
