897517a00f585664ac7281a0dfc821d9126b7815
Airuop <114985398+Airuop@users.noreply.github.com>
2023-09-02 12:09:07 +0330

    Update code.py

--- tools/code.py
+++ tools/code.py
@@ -5,7 +5,6 @@ import natsort
 
 paths = ['./countries/ir/2308', './update/2308', './donated/2308']
 
-
 def chunk_file(file_path, chunk_size=90*1024*1024):
   print(file_path)
 
@@ -35,65 +34,54 @@ def changeName(name, path):
         new_name = file.replace('-part1', '')
         os.rename(os.path.join(path, file), os.path.join(path, new_name))
 
-for path in paths:
+def get_all_paths(path):
+    all_files = []
+    for root, dirs, files in os.walk(path, topdown=False):
+      #print('root=%s , files=%s' %(root, files))
+      for file_name in files:
+        all_files.append(os.path.join(root, file_name))
+        #print(all_files)
+    return all_files
 
+def merge_files(all_files, output_file):
+    with open(output_file, 'a') as fout:
+      for file in all_files:
+        if file.endswith(".txt"):
+          with open(file) as fin:
+            fout.write(fin.read())
+            fout.write('\n')
+    
+
+for path in paths:
+  
+  output_file = f'{path}/integrated.txt'
+  output_file_final = f'{path}/final.txt'
   # Remove old final and integrated files
   for file in os.listdir(path):
     if 'integrated' in file or 'final' in file:
       file_path = os.path.join(path, file)
       os.remove(file_path)
 
-  output_file = f'{path}/integrated.txt'
-  output_file_final = f'{path}/final.txt'
-
-  def get_all_paths(path):
-    all_files = []
-    all_dirs = []
-
-    for dir_name, dir_names, file_names in os.walk(path, topdown=False):
-      for file_name in file_names:
-        all_files.append(os.path.join(dir_name, file_name))
-      all_dirs.append(dir_name)
-
-    return all_files, all_dirs
-
-  def merge_files(all_files, output_file):
-    with open(output_file, 'a') as fout:
-      for file in all_files:
-        if file.endswith(".txt"):
-          with open(file) as fin:
-            fout.write(fin.read())
-
-  all_files, all_dirs = get_all_paths(path)
+  all_files = get_all_paths(path)
   merge_files(all_files, output_file)
 
   with open(output_file) as f:
     lines = f.readlines()
+    
   chunk_file(output_file)
 
   keys = []
   for line in lines:
-    match = re.search(r'@([^:]+):\s*(\S+)', line)
+    print(line)
+    match = re.search(r'@([^?]+)?\s*(\S+)', line)
     if match:
       key = match.group(1)
     else:
-      key = ''
+      key = '0'
     keys.append(key)
-
+  #print('key = %s , keys=%s' %(key, keys))
   sorted_lines = [lines[keys.index(key)] for key in natsort.natsorted(keys)]
 
-  duplicates = []
-  seen = set()
-  regex = r'[@][^?]*[?]'
-
-  for line in sorted_lines:
-    match = re.search(regex, line)
-    if match:
-      val = match.group()
-      if val in seen:
-        duplicates.append(line)
-      else:
-        seen.add(val)
 
   deduped_lines = []
   for line in sorted_lines:
@@ -102,13 +90,12 @@ for path in paths:
 
   final_lines = []
   for line in deduped_lines:
-    protocols = ['vless://', 'trojan://', 'vmess://', 'ss://', 'ssr://']
+    protocols = ['vless://']
     for protocol in protocols:
       regex = re.compile(f"^{protocol}(.*)")
       match = regex.match(line)
       if match:
-        link = match.group(1)
-        final_lines.append(protocol + link)
+        final_lines.append(match.group())
 
   with open(output_file_final, 'w') as outfile:
     for line in final_lines:
