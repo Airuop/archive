bcc7e97c2feecf237a2d81a7212aa08e26a2f5b3
Airuop <114985398+Airuop@users.noreply.github.com>
2024-05-24 19:16:39 +0330

    Update code.py

--- tools/code.py
+++ tools/code.py
@@ -3,7 +3,7 @@ import re
 import natsort
 
 
-paths = ['./countries/ir/2312', './update/2312', './donated/2312']
+paths = ['./countries/ir/2401', './update/2401', './donated/2401']
 
 def chunk_file(file_path, chunk_size=90*1024*1024):
   print(file_path)
