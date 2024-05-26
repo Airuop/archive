7f339ecb4fd352fe2025da07a65a9c87af8476c0
Airuop <114985398+Airuop@users.noreply.github.com>
2024-05-26 21:33:52 +0330

    Update code.py

--- tools/code.py
+++ tools/code.py
@@ -4,7 +4,7 @@ import os
 
 
 # paths = ['./countries/ir/', './update/', './donated/', './selected/']
-paths = ['./update/2404']
+paths = ['./countries/']
 
 def chunk_file(file_path, chunk_size=90*1024*1024):
     print("chunk_file state")
