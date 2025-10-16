1f1cd4f747757aa362ebb9f7d85bf1510730fead
Belowsky <belowsky26@gmail.com>
2025-10-16 17:26:39 -0400

    Syntax refactoring

--- tools/backup1.py
+++ tools/backup1.py
@@ -8,29 +8,31 @@ update_path = './fail/'
 url = 'https://raw.githubusercontent.com/Airuop/vpnfail/refs/heads/main/subscription/plain'
 response = requests.get(url)
 while True:
-      time.sleep(10)
-      response = requests.get(url)
-      if response.ok:
-          break
+    time.sleep(10)
+    response = requests.get(url)
+    if response.ok:
+        break
+
 
 def backup(response):
-  date_dir = datetime.now().strftime("%y%m")
-  date_file = datetime.now().strftime("%y%m%d_%H%M") + "C"
+    date_dir = datetime.now().strftime("%y%m")
+    date_file = datetime.now().strftime("%y%m%d_%H%M") + "C"
 
-  try:
-    os.makedirs(f"{update_path}/{date_dir}", exist_ok=True)
-  except OSError:
-    print("Error creating backup directory")
-    return
+    try:
+        os.makedirs(f"{update_path}/{date_dir}", exist_ok=True)
+    except OSError:
+        print("Error creating backup directory")
+        return
 
-  file_path = f"{update_path}/{date_dir}/{date_file}.txt"
+    file_path = f"{update_path}/{date_dir}/{date_file}.txt"
 
-  try:  
-    with open(file_path, "w", encoding="utf-8") as f:
-      for line in response.text.splitlines():
-          line = line.replace('&amp;', '&')
-          f.write(line + '\n')
-  except OSError:
-    print("Error writing backup file")
+    try:
+        with open(file_path, "w", encoding="utf-8") as f:
+            for line in response.text.splitlines():
+                line = line.replace('&amp;', '&')
+                f.write(line + '\n')
+    except OSError:
+        print("Error writing backup file")
+
 
 backup(response)
